4c5372d3f1a814e755fc3d4fe42283c63f9b6c56
midou-bit <midou@DESKTOP-GEB5SA4>
2025-04-01 15:59:08 +0200

    activite7

--- Lol/src/App.jsx
+++ Lol/src/App.jsx
@@ -66,6 +66,15 @@ function App() {
     alert(`Le prix du produit avec l'id ${data.id} a été modifié`);
   };
 
+  const handleDeleteProduct = async (id) => {
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+      method: 'DELETE',
+    });
+
+    const data = await response.json();
+    alert(`Le produit avec l'id ${data.id} a été supprimé`);
+  };
+
   return (
     <Container className="mt-4">
       <Button variant="primary" onClick={handleAddProduct} className="mb-4">
@@ -90,8 +99,15 @@ function App() {
                 <Button
                   variant="success"
                   onClick={() => handlePartialUpdate(product.id)}
+                  className="mb-2"
                 >
-                  Modifie le prix du produit
+                  Modifier le prix du produit
+                </Button>
+                <Button
+                  variant="danger"
+                  onClick={() => handleDeleteProduct(product.id)}
+                >
+                  Supprimer le produit
                 </Button>
               </Card.Body>
             </Card>
