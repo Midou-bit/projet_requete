77e3c4e902ca627e84eaba39a4c6404ab7cec7b5
midou-bit <midou@DESKTOP-GEB5SA4>
2025-04-01 15:30:17 +0200

    activite4

--- Lol/src/App.jsx
+++ Lol/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import { Card, Container, Row, Col, Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
@@ -19,8 +19,34 @@ function App() {
     fetchProducts();
   }, []);
 
+  const handleAddProduct = async () => {
+    try {
+      const response = await fetch('https://fakestoreapi.com/products', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: 'Produit fictif',
+          price: 29.99,
+          description: 'Ceci est un produit factice pour test',
+          image: 'https://via.placeholder.com/150',
+          category: 'test',
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const data = await response.json();
+      alert(`Le produit avec l'id ${data.id} a été créé`);
+    } catch (error) {
+      console.error('Erreur lors de la création :', error);
+    }
+  };
+
   return (
     <Container className="mt-4">
+      <Button variant="primary" onClick={handleAddProduct} className="mb-4">
+        Ajouter un produit
+      </Button>
       <Row className="g-4">
         {products.map(product => (
           <Col xs={1} md={3} key={product.id}>
